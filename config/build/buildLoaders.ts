9b1ea6bd894c6b6ff654f087bc53667da8d5e367
Ira <nikulochkina.irina@yandex.ru>
2024-01-16 13:28:29 +0300

    добавили модули

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,6 +5,28 @@ import {BuildOptions} from './types/types';
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   const isDev = options.mode === 'development';
 
+  const svgrLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: [{
+      loader: '@svgr/webpack', options: {
+        icon: true, svgoConfig: {
+          plugins: [
+            {
+              name: 'convertColours',
+              params: {
+                currentColor: true,
+              }
+            }
+          ]
+        }
+      }
+    }],
+  };
+  const assetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  };
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
@@ -18,5 +40,7 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   return [
     scssLoader,
     tsLoader,
+    assetLoader,
+    svgrLoader
   ]
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,18 +2,20 @@ import webpack from 'webpack';
 import 'webpack-dev-server';
 import path from 'path';
 import {buildWebpack} from './config/build/buildWebpack';
-import {BuildMode, BuildPaths} from './config/build/types/types';
+import {BuildMode, BuildPaths, BuildPlatform} from './config/build/types/types';
 
 interface EnvVariables {
   mode: BuildMode;
   port: number;
+  platform: BuildPlatform;
 }
 
 export default (env: EnvVariables) => {
   const paths: BuildPaths = {
     output: path.resolve(__dirname, 'build'),
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    html: path.resolve(__dirname, 'public', 'index.html')
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src')
   }
   const config: webpack.Configuration = buildWebpack({
     port: env.port ?? 3000,
