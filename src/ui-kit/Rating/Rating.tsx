8ae78c14daf5a84e3a869a5466c099dab2076cf6
Ira <nikulochkina.irina@yandex.ru>
2024-04-10 13:05:37 +0300

    рейтинг дан

--- src/ui-kit/Rating/Rating.tsx
+++ src/ui-kit/Rating/Rating.tsx
@@ -2,27 +2,39 @@ import React, { useState } from 'react';
 
 import styles from './Rating.module.scss';
 import { ReactComponent as Star } from '../../assets/img/Star.svg';
+import { ReactComponent as StarFilled } from '../../assets/img/StarFilled.svg';
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
+  const handleStarClick = (index: number) => {
+    setRating(index + 1);
+  };
+
+  const handleStarHover = (index: number) => {
+    setHover(index + 1);
+  };
+
+  const handleStarLeave = () => {
+    setHover(0);
+  };
+
   return (
     <div className={styles.ratingContainer}>
-      {[...Array(5)].map((star, index) => {
+      {[...Array(5)].map((_, index) => {
+        const starFilled = index < (hover || rating);
 
         return (
-          <>
-            <button
-              type="button"
-              key={index}
-              className={index <= (hover || rating) ? 'on' : 'off'}
-              onClick={() => setRating(index)}
-              onMouseEnter={() => setHover(index)}
-              onMouseLeave={() => setHover(rating)}
-             />
-            <Star />
-          </>
+          <div
+            key={index}
+            className={styles.starContainer}
+            onClick={() => handleStarClick(index)}
+            onMouseEnter={() => handleStarHover(index)}
+            onMouseLeave={handleStarLeave}
+          >
+            {starFilled ? <StarFilled /> : <Star />}
+          </div>
         );
       })}
     </div>
